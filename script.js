216fb3fd3f747448cc0007be674c9d6e9d0f3bdf
BKShanmugaPriya <shanmugapriyabk579@gmail.com>
2025-12-31 23:57:25 +0530

    B.K.Shanmuga Priya
    Resume

---
+++ script.js
@@ -0,0 +1,7 @@
+alert('you are viewing Shanmuga Priya Resume');
+let now=new Date();
+let date=( `[${now.getDate()}/${now.getMonth()}/${now.getFullYear()}]`);
+document.writeln(date);
+function viewcertificate(){
+    window.open("certificate.jpeg","_blank");
+}
