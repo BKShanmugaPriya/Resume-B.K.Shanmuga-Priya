5f82b513b9f58fe174f7cce8022a36893473d768
BKShanmugaPriya <shanmugapriyabk579@gmail.com>
2025-12-31 23:59:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-alert('you are viewing Shanmuga Priya Resume');
+alert('You are viewing Shanmuga Priya Resume');
 let now=new Date();
 let date=( `[${now.getDate()}/${now.getMonth()}/${now.getFullYear()}]`);
 document.writeln(date);
 function viewcertificate(){
     window.open("certificate.jpeg","_blank");
+
 }
